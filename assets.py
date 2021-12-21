51722aa93fcb688e2d3878c6562c43467fa2028d
cmdvmd <55916643+cmdvmd@users.noreply.github.com>
2021-12-21 09:37:20 -0500

    Add assets

---
+++ assets.py
@@ -0,0 +1,7 @@
+import pygame
+import os
+
+ICON = pygame.image.load(os.path.join('assets', 'icon.png'))
+SMALL_PLANE = pygame.image.load(os.path.join('assets', 'small_plane.png'))
+MEDIUM_PLANE = pygame.image.load(os.path.join('assets', 'medium_plane.png'))
+LARGE_PLANE = pygame.image.load(os.path.join('assets', 'large_plane.png'))
